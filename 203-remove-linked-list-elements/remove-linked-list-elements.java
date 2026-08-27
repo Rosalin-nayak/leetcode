70fc7a5ec3d868e66c92d4f51350e189ce2107c6
Rosalin Nayak <rosalinnayak063@gmail.com>
2026-08-27 07:28:05 +0530

    Time: 1 ms (96.90%) | Memory: 47.7 MB (7.58%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode temp=head;
+        ListNode dummy=new ListNode(-1,head);
+        ListNode prev=dummy;
+        while(temp!=null){
+            if(temp.val==val){
+                prev.next=temp.next;
+                temp=temp.next;
+            }else{
+                temp=temp.next;
+                prev=prev.next;
+            }
+        }
+        return dummy.next;
+    }
+}
